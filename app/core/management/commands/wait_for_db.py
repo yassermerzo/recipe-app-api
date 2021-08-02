77432f809cee1b709cc744354e3738b2c2fcdf7d
yassermerzo <yassermerzouki@gmail.com>
2021-08-02 18:17:59 +0100

    test 103

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
